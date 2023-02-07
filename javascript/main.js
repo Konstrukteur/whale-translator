a83cdc372824868eb23e3ef92cb8fbdb963bae53
KONSTRUKTEUR <raumerkunder@gmail.com>
2023-02-07 20:39:43 +0100

    First cut translator

---
+++ javascript/main.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelector("#translate-button").addEventListener("click", () => {
+    const vowels = ["a", "e", "i", "o", "u"];
+    let resultArray = [];
+
+    let input = (() => {
+      content = document.querySelector("#human-text").value;
+      return content;
+    });
+
+    let testForVowels = vowels.forEach((element) => {
+      return element;
+    });
+
+    Array.from(input()).forEach((element, index) => {
+      if (element == "e") {
+        resultArray.push(element)
+      } else if (element == "u") {
+        resultArray.push(element)
+      }
+      vowels.forEach((el) => {
+        if (element == el) {
+          resultArray.push(element);
+          console.log(element, index);
+        };
+      });
+    });
+
+    whaleSpeech = resultArray.join("").toUpperCase();
+
+    document.querySelector("#whale-text").innerHTML = whaleSpeech;
+  });
+});
